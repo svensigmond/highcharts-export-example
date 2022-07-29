387e115b80a2d16dcd9286a9d3655bc029a75d9c
Sven Sigmond <sven@vicompany.nl>
2022-07-29 10:16:20 +0200

    Add highcharts example and update readme

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,59 @@
 <template>
-	<div ref="container"></div>
+  <div ref="container"></div>
 </template>
 
 <script>
-	export default {
-	name: 'HomePage'
-	}
+import Highcharts from "highcharts/highcharts.src";
+import exporting from "highcharts/modules/exporting";
+
+export default {
+  name: "HomePage",
+
+  mounted() {
+	exporting(Highcharts);
+
+    Highcharts.chart(this.$refs.container, {
+      chart: {
+        type: "column",
+        styledMode: true,
+      },
+
+      title: {
+        text: "Styling axes and columns",
+      },
+
+      yAxis: [
+        {
+          className: "highcharts-color-0",
+          title: {
+            text: "Primary axis",
+          },
+        },
+        {
+          className: "highcharts-color-1",
+          opposite: true,
+          title: {
+            text: "Secondary axis",
+          },
+        },
+      ],
+
+      plotOptions: {
+        column: {
+          borderRadius: 5,
+        },
+      },
+
+      series: [
+        {
+          data: [1, 3, 2, 4],
+        },
+        {
+          data: [324, 124, 547, 221],
+          yAxis: 1,
+        },
+      ],
+    });
+  },
+};
 </script>
